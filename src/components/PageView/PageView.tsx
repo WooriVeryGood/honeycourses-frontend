48a29d08054dcf475ef12a11b5cf314af7148c75
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-05-09 01:13:36 +0800

    Bootstrap 적용, Header기초 레이아웃

---
+++ src/components/PageView/PageView.tsx
@@ -0,0 +1,11 @@
+import './PageView.css'
+
+export default function PageView() {
+  return (
+    <div className="body">
+      <div className="alert alert-info d-flex align-items-center" role="alert">
+        안뇽! 이제 시작이야!
+      </div>
+    </div>
+  );
+}
