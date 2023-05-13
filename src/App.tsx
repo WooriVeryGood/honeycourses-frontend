478a80a893238134f19c83707685e280b8c481ce
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-05-14 00:58:41 +0800

    애널리틱스 기능 부활 (버그 잡음)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Header from "./components/Header/Header";
@@ -10,10 +10,24 @@ import CourseList from "./components/CourseList/CourseList";
 import CourseReviews from "./components/CourseReviews/CourseReviews";
 import AddReview from "./components/AddReview/AddReview";
 import AddCourse from "./components/AddCourse/AddCourse";
+import ReactGA from 'react-ga';
 
 function App() {
+  function GoogleAnalytics() {
+    const location = useLocation();
+
+    useEffect(() => {
+      ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID as string);
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    }, [location]);
+
+    return null;
+  }
+
   return (
     <BrowserRouter>
+    <GoogleAnalytics />
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
