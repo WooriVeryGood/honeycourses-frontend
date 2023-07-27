f2ffa2e1d16cfca3d8b5030baad4f5275b42fed7
Joonwoo Jang (Percy) <joonwoo_jang@pku.edu.cn>
2023-07-27 17:07:51 +0800

    로그인 화면 수정

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,22 +1,30 @@
 import { useEffect } from "react";
-import { Authenticator, useAuthenticator, View } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import { useNavigate, useLocation } from 'react-router';
+import { Authenticator, useAuthenticator, View } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { useNavigate, useLocation } from "react-router";
+import PageView from "../PageView/PageView";
+import Container from "react-bootstrap/esm/Container";
 
 export function Login() {
   const { route } = useAuthenticator((context) => [context.route]);
   const location = useLocation();
   const navigate = useNavigate();
-  let from = location.state?.from?.pathname || '/';
+  let from = location.state?.from?.pathname || "/";
   useEffect(() => {
-    if (route === 'authenticated') {
+    if (route === "authenticated") {
       navigate(from, { replace: true });
     }
   }, [route, navigate, from]);
   return (
-    <View className="auth-wrapper">
-      <Authenticator></Authenticator>
-    </View>
+    <PageView>
+        <Container
+          fluid
+          className="justify-content-center align-items-start"
+        >
+      <View className="auth-wrapper">
+        <Authenticator></Authenticator>
+      </View>
+      </Container>
+    </PageView>
   );
 }
