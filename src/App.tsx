b97cdeac879b201b9aa6be099d455b42a6445a86
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-01-14 20:19:30 +0800

    [HC-78] 커뮤니티 기능 재오픈 (숨김 취소)

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import AddReview from "./components/AddReview/AddReview";
 import AddCourse from "./components/AddCourse/AddCourse";
 import TermsConditions from "./components/TermsConditions/TermsConditions";
 import Support from "./components/Support/Support";
-// import CommunityHome from "./components/CommunityHome/CommunityHome";
+import CommunityHome from "./components/CommunityHome/CommunityHome";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
@@ -19,8 +19,8 @@ import { I18n } from 'aws-amplify';
 import { translations } from '@aws-amplify/ui-react';
 import { RequireAuth } from "./RequireAuth";
 import { Login } from "./components/Login/Login";
-// import CommunityPostView from "./components/CommunityPostView/CommunityPostView";
-// import AddPost from "./components/AddPost/AddPost";
+import CommunityPostView from "./components/CommunityPostView/CommunityPostView";
+import AddPost from "./components/AddPost/AddPost";
 I18n.putVocabularies(translations);
 I18n.setLanguage('ko');
 Amplify.configure(awsExports);
@@ -55,10 +55,9 @@ export default function App() {
             <Route path="/courses/view/:classID" element={<RequireAuth><CourseReviews /></RequireAuth>} />
             <Route path="/courses/addReview/:classID" element={<RequireAuth><AddReview /></RequireAuth>} />
             <Route path="/courses/addCourse" element={<RequireAuth><AddCourse /></RequireAuth>} />
-            {/*<Route path="/community" element={<RequireAuth><CommunityHome /></RequireAuth>} />
+            <Route path="/community" element={<RequireAuth><CommunityHome /></RequireAuth>} />
             <Route path="/community/view/:postID" element={<RequireAuth><CommunityPostView /></RequireAuth>} />
             <Route path="/community/addPost" element={<RequireAuth><AddPost /></RequireAuth>} />
-            */}
             <Route path="/termsConditions" element={<TermsConditions />} />
             <Route path="/support" element={<Support />} />
             <Route path="/login" element={<Login />} />
