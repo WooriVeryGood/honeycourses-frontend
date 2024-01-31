3b9f3d3ff3733efeb48db84fff9f9d116d849c6b
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-02-01 01:15:47 +0800

    [HC-136] 52명의 다른 댓글 사용자 수용하도록 닉네임 확장

--- src/components/CommunityPostView/CommunityPostView.tsx
+++ src/components/CommunityPostView/CommunityPostView.tsx
@@ -53,7 +53,7 @@ interface SystemError {
   message: string;
 }
 
-const pseudonyms = [
+const pseudonymsSample = [
   "Alice",
   "Bob",
   "Carol",
@@ -82,7 +82,38 @@ const pseudonyms = [
   "Zach",
 ];
 
+const pseudonyms = [...pseudonymsSample, ...pseudonymsSample.map(name => `Angry ${name}`)]; //52명까지 우선 수용
+
+
 const commentBackgroundColors = [
+  "#91C8E4", //파
+  "#EAC696", //갈
+  "#C8E4B2", //초
+  "#D8D9DA", //회
+  "#FFE17B", //노
+  "#DFCCFB", //보
+  "#E19898", //핑
+  "#33BBC5", //파
+  "#A8DF8E", //초
+  "#EBE76C", //겨
+  "#5C5470", //짙회
+  "#A6E3E9", //하
+  "#E3FDFD", //연하
+  "#A6B1E1", //연남
+  "#d9e1fc",
+  "#edd9f2",
+  "#d9f2e5",
+  "#FFD54F",
+  "#A1887F",
+  "#FFAB91",
+  "#FFCC80",
+  "#FFF176",
+  "#DCE775",
+  "#AED581",
+  "#81C784",
+  "#4FC3F7",
+  "#4DD0E1",
+  "#4DB6AC",
   "#91C8E4", //파
   "#EAC696", //갈
   "#C8E4B2", //초
