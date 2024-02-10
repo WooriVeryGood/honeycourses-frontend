bfe5df80311cc637d08532ce9cd247eee07587a3
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-02-11 01:30:06 +0800

    fix: 기본 UI 수정, 홈페이지 UI 수정

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -18,108 +18,92 @@ export default function Header() {
     location.pathname === "/" ? "/images/logo.png" : "/images/logowhite.png";
 
   return (
-    <Navbar
-      expand="lg"
-      fixed="top"
-      style={{
-        backgroundSize: "0",
-        height: "70px",
-        backgroundColor,
-        paddingLeft: "0",
-        paddingRight: "0",
-      }}
-      className={styles.headerBody}
-    >
-      <Container fluid className="p-0">
-        <Navbar.Brand
-          href="/"
-          style={{
-            color: "black",
-            display: "flex",
-            alignItems: "center",
-            marginLeft: "2%",
-          }}
-        >
-          <img
-            alt=""
-            src={logoImage}
-            width="50"
-            height="50"
-            style={{ marginRight: "2%" }}
-            className="d-inline-block align-top"
+    <nav>
+      <Navbar
+        className={styles.navBar}
+        expand="lg"
+        fixed="top"
+        style={{
+          backgroundColor,
+        }}
+      >
+        <Container fluid className="p-0" style={{maxWidth: "100vw"}}>
+          <Navbar.Brand href="/" className={styles.navBarBrand}>
+            <img
+              alt=""
+              src={logoImage}
+              width="50"
+              height="50"
+              style={{ marginRight: "2%" }}
+              className="d-inline-block align-top"
+            />
+            <span>답변 받았습니다!</span>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ marginRight: "5%" }}
           />
-          <span>답변 받았습니다!</span>
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          style={{ marginRight: "10%" }}
-        />
-        <Navbar.Collapse>
-          <Nav
-            className="me-auto"
-            style={{ backgroundColor, paddingRight: "20px" }}
-          >
-            <Nav.Link className="text text-center" href="/">
-              Home
-            </Nav.Link>
-            <Nav.Link className="text text-center" href="/about">
-              About
-            </Nav.Link>
-            <Nav.Link className="text text-center" href="/courses">
-              Courses
-            </Nav.Link>
-            <Nav.Link className="text text-center" href="/community">
-              Community
-            </Nav.Link>
-            <Nav.Link
-              className="text text-center"
-              href="https://scores.honeycourses.com"
-            >
-              Scores
-            </Nav.Link>
-            <Nav.Link className="text text-center" href="/support">
-              Support Us!
-            </Nav.Link>
-          </Nav>
-          <div
-            className="text text-center"
-            style={{ backgroundColor, paddingRight: "20px" }}
-          >
-            {authStatus !== "authenticated" ? (
-              <>
-                <p className="d-inline me-3 text text-center">
-                  로그인해주세요.
-                </p>
-                <Button
-                  className="text text-center"
-                  variant="success"
-                  href="/login"
-                >
-                  로그인
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button
-                  className="text text-center"
-                  variant="primary"
-                  href="/myinfo"
-                  style={{ marginRight: "10px" }}
-                >
-                  내 정보
-                </Button>
-                <Button
-                  className="text text-center"
-                  variant="danger"
-                  onClick={signOut}
-                >
-                  로그아웃
-                </Button>
-              </>
-            )}
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <Navbar.Collapse>
+            <Nav className="me-auto" style={{ backgroundColor }}>
+              <Nav.Link className="text text-center" href="/">
+                Home
+              </Nav.Link>
+              <Nav.Link className="text text-center" href="/about">
+                About
+              </Nav.Link>
+              <Nav.Link className="text text-center" href="/courses">
+                Courses
+              </Nav.Link>
+              <Nav.Link className="text text-center" href="/community">
+                Community
+              </Nav.Link>
+              <Nav.Link
+                className="text text-center"
+                href="https://scores.honeycourses.com"
+              >
+                Scores
+              </Nav.Link>
+              <Nav.Link className="text text-center" href="/support">
+                Support Us!
+              </Nav.Link>
+            </Nav>
+            <div className={styles.buttonArea} style={{ backgroundColor }}>
+              {authStatus !== "authenticated" ? (
+                <>
+                  <p className="d-inline me-3 text text-center">
+                    로그인해주세요.
+                  </p>
+                  <Button
+                    className="text text-center"
+                    variant="success"
+                    href="/login"
+                  >
+                    로그인
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button
+                    className="text text-center"
+                    variant="primary"
+                    href="/myinfo"
+                    style={{ marginRight: "10px" }}
+                  >
+                    내 정보
+                  </Button>
+                  <Button
+                    className="text text-center"
+                    variant="danger"
+                    onClick={signOut}
+                  >
+                    로그아웃
+                  </Button>
+                </>
+              )}
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </nav>
   );
 }
