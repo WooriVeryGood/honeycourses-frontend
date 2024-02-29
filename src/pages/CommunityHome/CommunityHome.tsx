6938e04090d8793b0153dc3d02db1b5ebfa22cb3
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-03-01 01:30:45 +0800

    [HC-169] CommunityHome 게시글 불러오기 React-query 적용완료

--- src/pages/CommunityHome/CommunityHome.tsx
+++ src/pages/CommunityHome/CommunityHome.tsx
@@ -1,73 +1,41 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import PageView from "../PageView/PageView";
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { apiGet } from "../../API/APIHandler";
-import { CategoryKey, Post } from "../../types/post";
+import { CategoryKey } from "../../types/post";
 
 import Sidebar from "./components/Sidebar/CourseSideBar";
 import PostContainer from "./components/PostContainer/PostContainer";
-import { POST_CATEGORY_LABELS } from "../../constants/texts";
 
 import styles from './CommunityHome.module.css';
+import { usePosts } from "../../API/posts/usePosts";
+import Loader from "../../components/Loader/Loader";
 
 export default function CommunityHome() {
   const savedPage = localStorage.getItem("lastPage");
   const savedCategory = localStorage.getItem("lastCategory");
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [noticePosts, setNoticePosts] = useState<Post[]>([]);
   const [currentCategory, setcurrentCategory] = useState<CategoryKey>((savedCategory === null) ? "All" : savedCategory as CategoryKey);
   const [currentPage, setCurrentPage] = useState<number>((savedPage === null) ? 1 : Number(savedPage));
-  const [totalItemsCount, setTotalItemsCount] = useState(0);
-  const isFirstMount = useRef(true);
-
   const navigate = useNavigate();
 
-  const fetchDataFromApi = async (pageNo: number, category: string = "") => {
-    try {
-      setIsLoading(true);
-      const categoryPath = category ? `/category/${category}` : "";
-      const noticeResponse = await apiGet(`/community/category/notice`);
-      const response = await apiGet(`/community${categoryPath}?page=${pageNo - 1}`);
-      setNoticePosts(noticeResponse.data.posts);
-      setPosts(response.data.posts);
-      setTotalItemsCount(response.data.totalPostCount);
-      setIsLoading(false);
-      window.scrollTo(0, 0);
-    } catch (error) {
-      console.error(error);
-      setIsLoading(false);
-      window.scrollTo(0, 0);
-    }
-  };
+  const {isLoading, error, posts, noticePosts, totalItemsCount} = usePosts(currentPage, currentCategory);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [currentPage]);
+
+  if (!posts || !noticePosts || isLoading) {
+    return <Loader />;
+  }
+
+  if(error) {
+    alert('게시글 불러오기에 실패했습니다.');
+  }
 
   const handleSelectCategory = (category: CategoryKey) => {
     setCurrentPage(1);
     setcurrentCategory(category);
-    fetchDataFromApi(1, POST_CATEGORY_LABELS[category]);
   };
-
-  useEffect(() => {
-    if (isFirstMount.current) {
-      isFirstMount.current = false; 
-  
-      const savedPage = localStorage.getItem("lastPage");
-      const savedCategory = localStorage.getItem("lastCategory");
-      if (savedPage !== null && savedCategory !== null) {
-        setCurrentPage(Number(savedPage));
-        setcurrentCategory(savedCategory as CategoryKey);
-
-        localStorage.removeItem("lastPage");
-        localStorage.removeItem("lastCategory");
-
-      }
-    }
-    if (!isFirstMount.current || (savedPage === null && savedCategory === null)) {
-      fetchDataFromApi(currentPage, POST_CATEGORY_LABELS[currentCategory]);
-    }
-  }, [currentPage]);
 
   const linkToPostView = (postId: number) => {
     localStorage.setItem("lastPage", String(currentPage));
