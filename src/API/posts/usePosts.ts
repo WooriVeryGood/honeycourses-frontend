ea2b05c0f11ecde8cda44c8a56f43adc0e868c06
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-03-01 01:32:52 +0800

    [HC-196] 주석 수정

--- src/API/posts/usePosts.ts
+++ src/API/posts/usePosts.ts
@@ -13,7 +13,7 @@ export function usePosts(pageNo: number, category: string = "") {
     staleTime: 1 * 60 * 1000,
     retry: (failCount, error) => {
       const axiosError = error as AxiosError;
-      if (axiosError?.response?.status === 404) return false; // 존재하지 않는 강의는 재시도 하지 않음
+      if (axiosError?.response?.status === 404) return false; // 존재하지 않는 페이지/카테고리의 요청은 재시도하지 않음
       return failCount < 3;
     },
   });
